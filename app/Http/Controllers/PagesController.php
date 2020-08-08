2c68bf625764479e586fe8c888bd7005788a3c38
daniz130297 <daniz130297@gmail.com>
2020-08-08 22:23:22 +0700

    init comit

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\User;
+
+class PagesController extends Controller
+{
+   
+    public function home()
+    {
+        return view('index');
+    }
+
+    public function about()
+    {
+        return view('about',['nama' =>'Dani Setiawan']);
+
+    }
+}
---
+++ resources/views/index.blade.php
@@ -0,0 +1,17 @@
+@extends('layout/main')
+
+
+
+@section('title','WebProgramming UNPAS')
+
+@section('container')
+
+    <div class="container">
+      <div class="row">
+        <div class="col-10">
+          <h1 class="mt-3">KDE</h1>
+</div>
+  </div>
+      </div>
+@endsection
+    
